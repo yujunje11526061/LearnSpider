d47427b33bcba3187942cc8727f055b450c17443
Junjie_Yu <yujunjie@zju.edu.cn>
2018-11-06 23:46:18 +0800

    add note

--- 基础爬虫/xpath解析深圳短租房.py
+++ 基础爬虫/xpath解析深圳短租房.py
@@ -6,11 +6,12 @@ import pandas as pd
 
 r = requests.get('http://sz.xiaozhu.com/').text
 s = etree.HTML(r)
-price = s.xpath('//*[@id="page_list"]/ul/li/div[2]/span[1]/i/text()')
+price = s.xpath('//*[@id="page_list"]/ul/li/div[2]/span[1]/i/text()') # *表示通配符，表示所有标签，可以指定某个特定标签下取属性
 latlng = s.xpath('//*[@id="page_list"]/ul/li/@latlng')
 address = s.xpath('//*[@id="page_list"]/ul/li/div[2]/div/a/span/text()')
 # print(latlng)
 # print(address)
 data = {'latlng':latlng, 'price':price}
 df = pd.DataFrame(data, index=address).sort_values('price')
+
 df.to_csv('C:\\Users\\jjff1\\Desktop\\pr.csv', encoding = 'utf_8_sig')
